4e26e46da5c0d90d85ab9d487d435da829331bfa
ignacio <igarciap@fi.uba.ar>
2023-11-16 20:20:12 -0300

    controller del ticket

--- App/controllers/ticket.py
+++ App/controllers/ticket.py
@@ -1,7 +1,27 @@
 from fastapi import APIRouter
 
-router = APIRouter()
+router = APIRouter(
+    prefix="/ticket",
+    tags=["ticket"],
+    responses={404: {"description": "Not found"}},
+)
 
-@router.get("/tickets")
+@router.get("/")
 async def tickets():
+    return {"message": "Hello World"}
+
+@router.post("/")
+async def create_ticket():
+    return {"message": "Hello World"}
+
+@router.get("/{ticket_id}")
+async def get_ticket(ticket_id: int):
+    return {"message": "Hello World"}
+
+@router.put("/{ticket_id}")
+async def update_ticket(ticket_id: int):
+    return {"message": "Hello World"}
+
+@router.delete("/{ticket_id}")
+async def delete_ticket(ticket_id: int):
     return {"message": "Hello World"}
--- App/main.py
+++ App/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from controllers import ticket
 
+
 app = FastAPI()
+
+# CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Endpoints
 app.include_router(ticket.router)
-
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+#crear ticket
