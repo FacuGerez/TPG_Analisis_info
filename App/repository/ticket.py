517a6f4e43ced5709b600540672b67c9adac1557
FacuGerez <facundogerezuba@hotmail.com>
2023-11-16 17:27:34 -0300

    Repository actualizado

--- App/repository/ticket.py
+++ App/repository/ticket.py
@@ -1,4 +1,5 @@
 from sqlalchemy.orm import Session
+
 from ..database.ticket import Ticket
 
 
@@ -8,13 +9,13 @@ class TicketRepository:
     def __init__(self, db: Session):
         self.db: Session = db
 
-    def get_ticket(self, ticket_id: int):
+    def get_ticket(self, ticket_id: int) -> Ticket:
         return self.db.query(Ticket).get(ticket_id)
 
-    def get_tickets(self):  # skip: int = 0, limit: int = 100 para poner un limite
+    def get_tickets(self) -> list[[Ticket]]:  # skip: int = 0, limit: int = 100 para poner un limite
         return self.db.query(Ticket).all()
 
-    def save_tickets(self, ticket: Ticket):
+    def save_tickets(self, ticket: Ticket) -> Ticket:
         self.db.add(ticket)
         self.db.commit()
         self.db.refresh(ticket)
