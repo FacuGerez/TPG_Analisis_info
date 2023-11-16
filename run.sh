308b39a55ed5f4ae0a51bb388525eb3fb4a81cfa
ignacio <igarciap@fi.uba.ar>
2023-11-16 19:53:57 -0300

    cambio un poco el run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+# Install python3-venv
+sudo apt-get install python3.10-venv
+
+# Create a virtual environment
+python -m venv venv
+
+# Activate the virtual environment
+source ./venv/bin/activate
+
 # Install the requirements
 pip install -r requirements.txt
 
 # Run the FastAPI application
-cd App
-uvicorn main:app --reload 
+cd App || exit
+uvicorn main:app --reload
