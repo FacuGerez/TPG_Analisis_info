fabd4b2d283fbb3839db90a73ccebec6abb88ab3
ignacio <igarciap@fi.uba.ar>
2023-11-16 21:36:35 -0300

    client controller + link database

--- App/database/database.py
+++ App/database/database.py
@@ -2,6 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+# https://planetscale.com/blog/using-mysql-with-sql-alchemy-hands-on-examples
+
 SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
