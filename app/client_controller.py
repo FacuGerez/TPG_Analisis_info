93a2757421c84e732fbfd99dc771ad78e00cba3e
FacuGerez <facundogerezuba@hotmail.com>
2023-11-30 23:14:28 -0300

    Arreglo de error de client_controller.py

--- app/client_controller.py
+++ app/client_controller.py
@@ -11,4 +11,4 @@ router = APIRouter(
 def clients():
     with Session() as sess:
         response = sess.get("https://anypoint.mulesoft.com/mocking/api/v1/sources/exchange/assets/754f50e8-20d8-4223-bbdc-56d50131d0ae/clientes-psa/1.0.0/m/api/clientes")
-        return response
+        return response.json()
