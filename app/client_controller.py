e71b6e08002772eb7eaf4c43f6d74fd0a7ce4e76
VickyA5 <vavalos@fi.uba.ar>
2023-11-30 22:55:36 -0300

    cambio client controller

--- app/client_controller.py
+++ app/client_controller.py
@@ -10,6 +10,5 @@ router = APIRouter(
 @router.get("/product/{product_id}/client")
 def clients():
     with Session() as sess:
-        response = sess.get("https://anypoint.mulesoft.com/mocking/api/v1/sources/exchange/assets/754f50e8-20d8-4223"
-                            "-bbdc-56d50131d0ae/clientes-psa/1.0.0/m/api/clientes")
+        response = sess.get("https://anypoint.mulesoft.com/mocking/api/v1/sources/exchange/assets/754f50e8-20d8-4223-bbdc-56d50131d0ae/clientes-psa/1.0.0/m/api/clientes")
         return response
